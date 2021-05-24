83620be37753c07a3524068b5b0e9a34d82abc12
rhodenisabella7 <84419465+rhodenisabella7@users.noreply.github.com>
2021-05-24 16:48:34 -0500

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,28 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = this.pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    
+    fly(){
+        this.sling.bodyA = null;
+    }
+    
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            strokeWeight(4);
+            line(pointA.x, pointA.y, 200, 100);
+            pop();
+        }
+    }
+}
